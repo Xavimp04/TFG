f3a2687857ef115762f319d945ed14a697f3f7ca
Xavier Marzo Pelaz <145644964+Xavimp04@users.noreply.github.com>
2026-01-26 17:02:41 +0100

    Corregido funciones no creadas

---
+++ ForensicXM/src/analyzer_users.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "forensics.h"
+
+void analizar_usuarios() {
+    printf("\n[!] Módulo de usuarios en desarrollo...\n");
+}
---
+++ ForensicXM/src/persistence.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../include/forensics.h"
+
+void analizar_persistencia() {
+    printf("\n[!] Módulo de persistencia en desarrollo...\n");
+}
